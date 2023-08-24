8c89f834c263b7936c0554fcc42cdee8089c84f7
Hwi <124661622+Imhwitae@users.noreply.github.com>
2023-08-24 18:12:55 +0900

    프로젝트 생성

---
+++ DoToday/src/main/java/com/todolist/DoToday/DoTodayApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.DoToday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoTodayApplication.class, args);
+	}
+
+}
---
+++ DoToday/src/test/java/com/todolist/DoToday/DoTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.DoToday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
