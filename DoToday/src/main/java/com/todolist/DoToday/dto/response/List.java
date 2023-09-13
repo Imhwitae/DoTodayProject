fe626ebda3062438c18dc6b12fc95c6b4bccb7fb
seongmiin00 <cor2580@gmail.com>
2023-09-14 00:17:53 +0900

    todo리스트 dto 작성

---
+++ DoToday/src/main/java/com/todolist/DoToday/dto/response/List.java
@@ -0,0 +1,10 @@
+package com.todolist.DoToday.dto.response;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class List {
+    private String listTitle;
+    private String memberId;
+}
