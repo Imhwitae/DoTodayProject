ec679b203eba656df296c9be40fc5a6cba35f565
seongmiin00 <cor2580@gmail.com>
2023-09-25 16:10:13 +0900

    올림

---
+++ DoToday/src/main/java/com/todolist/DoToday/dto/request/FriendInfoDto.java
@@ -0,0 +1,10 @@
+package com.todolist.DoToday.dto.request;
+
+import lombok.Data;
+
+@Data
+public class FriendInfoDto {
+    private String memberId;
+    private String memberName;
+    private String memberImage;
+}
