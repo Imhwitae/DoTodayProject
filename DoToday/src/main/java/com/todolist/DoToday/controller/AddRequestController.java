69b459d8c54beb723687c110c24456b44fd11eaf
seongmiin00 <cor2580@gmail.com>
2023-10-21 21:48:50 +0900

    친구, 수락 컨트롤러 수정

--- DoToday/src/main/java/com/todolist/DoToday/controller/AddRequestController.java
+++ DoToday/src/main/java/com/todolist/DoToday/controller/AddRequestController.java
@@ -4,9 +4,12 @@ import com.todolist.DoToday.dto.request.AddRequest;
 import com.todolist.DoToday.dto.request.FriendInfoDto;
 import com.todolist.DoToday.dto.response.FriendList;
 import com.todolist.DoToday.dto.response.MemberDetailDto;
+import com.todolist.DoToday.jwt.JwtTokenProvider;
 import com.todolist.DoToday.service.AddRequestService;
 import com.todolist.DoToday.service.FriendService;
 import com.todolist.DoToday.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,20 +23,39 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/request")
+//친구 신청 목록 컨트롤러
 public class AddRequestController {
     private final AddRequestService addRequestService;
     private final MemberService memberService;
     private final FriendService friendService;
+    private final JwtTokenProvider jtp;
 
+    //자신에게온 신청 목록 불러오기
     @GetMapping("/list")
-    public String requestList(Model model){
-        List<FriendInfoDto> requests = addRequestService.selectRequestList("cor2580");
-        int requestCount = addRequestService.listCount("cor2580");
+    public String requestList(HttpServletRequest request, Model model){
+        Cookie[] cookies = request.getCookies();
+
+        MemberDetailDto mdd = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                String tokenName = cookie.getName();
+                String value = cookie.getValue();
+
+                if (tokenName.equals("accessToken")) {
+                    mdd = jtp.getMember(value);
+                }
+            }
+        }
+        List<FriendInfoDto> requests = addRequestService.selectRequestList(mdd.getMemberId());
+        int requestCount = addRequestService.listCount(mdd.getMemberId());
+
+        model.addAttribute("userInfo", mdd);
         model.addAttribute("requests",requests);
         model.addAttribute("rCount", requestCount);
-        return "test/request_test";
+        return "friend/request_test";
     }
 
+    //친구신청 수락
     @PostMapping("/accept")
     public String requestAccept(@RequestParam("friendId") String friendId){
         AddRequest add = new AddRequest();
@@ -43,6 +65,7 @@ public class AddRequestController {
         return "redirect:/request/list";
     }
 
+    //차단
     @PostMapping("/block")
     public String block(@RequestParam("friendId") String friendId){
         AddRequest add = new AddRequest();
@@ -52,6 +75,7 @@ public class AddRequestController {
         return "redirect:/request/list";
     }
 
+    //거절
     @PostMapping("/denied")
     public String requestDenied(@RequestParam("friendId") String friendId){
         AddRequest add = new AddRequest();
@@ -61,14 +85,16 @@ public class AddRequestController {
         return "redirect:/request/list";
     }
 
+    //유저 검색 페이락
     @GetMapping("/search")
     public String searchForm(Model model){
         model.addAttribute("exist",0);
 //        FriendInfoDto fid = new FriendInfoDto();
 //        model.addAttribute("memberInfo", fid);
-        return "test/add_form_test";
+        return "friend/add_form_test";
     }
 
+    //검색
     @PostMapping("/search")
     public String search(@RequestParam("memberId") String memberId, Model model){
         FriendList fl = new FriendList();
--- DoToday/src/main/java/com/todolist/DoToday/controller/FriendController.java
+++ DoToday/src/main/java/com/todolist/DoToday/controller/FriendController.java
@@ -3,8 +3,12 @@ package com.todolist.DoToday.controller;
 import com.todolist.DoToday.dto.request.AddRequest;
 import com.todolist.DoToday.dto.request.FriendInfoDto;
 import com.todolist.DoToday.dto.response.FriendList;
+import com.todolist.DoToday.dto.response.MemberDetailDto;
+import com.todolist.DoToday.jwt.JwtTokenProvider;
 import com.todolist.DoToday.service.AddRequestService;
 import com.todolist.DoToday.service.FriendService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -17,31 +21,56 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/friend")
+//친구 목록 컨트롤러
 public class FriendController {
     private final FriendService friendService;
     private final AddRequestService addRequestService;
+    private final JwtTokenProvider jtp;
 
+    //친구 리스트
     @GetMapping("/list")
-    public String friendList(Model model,@ModelAttribute("friend") FriendList friendList){
-        List<FriendInfoDto> infoDtoList = friendService.info("cor2580");
-        int listCount = friendService.listCount("cor2580");
-        int requestCount = addRequestService.listCount("cor2580");
+    public String friendList(HttpServletRequest request,
+                             Model model, @ModelAttribute("friend") FriendList friendList){
+        Cookie[] cookies = request.getCookies();
+
+        MemberDetailDto mdd = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                String tokenName = cookie.getName();
+                String value = cookie.getValue();
+
+                if (tokenName.equals("accessToken")) {
+                    mdd = jtp.getMember(value);
+                }
+            }
+        }
+
+        List<FriendInfoDto> infoDtoList = friendService.info(mdd.getMemberId());
+
+        int listCount = friendService.listCount(mdd.getMemberId());
+        int requestCount = addRequestService.listCount(mdd.getMemberId());
+
+        model.addAttribute("userDto",mdd);
         model.addAttribute("friendList", infoDtoList);
         model.addAttribute("lCount", listCount);
         model.addAttribute("rCount", requestCount);
-        return "test/friendlist_test";
+        return "friend/friendlist_test";
     }
 
+    //친구 삭제
     @PostMapping("/delete")
     public String deleteFriend(@RequestParam("userId") String userId,
                                @RequestParam("friendId") String friendId){
         FriendList friendList = new FriendList();
+        log.info(friendId);
+        log.info(userId);
         friendList.setFriendId(friendId);
         friendList.setUserId(userId);
         friendService.deleteFriend(friendList);
         return "redirect:/friend/list";
     }
 
+    //친구 신청 수락
     @PostMapping("/apply")
     public String applyFriend(@RequestParam("friendId") String friendId){
         AddRequest add = new AddRequest();
